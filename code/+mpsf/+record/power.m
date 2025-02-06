5c148f35f6997c3e1188283bce3b388ebc9388a6
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2025-02-06 17:12:53 +0000

    added sibridge test, updated class to call powermeter, added scanimage point, recording multiple data points at each percent power, removed parts of save that do not apply

--- code/+mpsf/+record/power.m
+++ code/+mpsf/+record/power.m
@@ -11,66 +11,82 @@ function power(varargin)
 % Isabell Whiteley, SWC 2025
 
 
-    out =  parseInputVariable(varargin{:});
-    laser_wavelength=out.wavelength;
+out =  parseInputVariable(varargin{:});
+laser_wavelength=out.wavelength;
 
 % Connect to ScanImage using the linker class
-    API = sibridge.silinker;
+API = sibridge.silinker;
 
- % Create 'diagnostic' directory in the user's desktop
-    saveDir = mpsf.tools.makeTodaysDataDirectory;
-    if isempty(saveDir)
-        return
-    end
+if API.linkSucceeded == false
+    return
+end
 
-    %Record the state of all ScanImage settings we will change so we can change them back
-    settings = mpsf.tools.recordScanImageSettings(API);
+% Create 'diagnostic' directory in the user's desktop
+saveDir = mpsf.tools.makeTodaysDataDirectory;
+if isempty(saveDir)
+    return
+end
 
-    API.turnOffPMTs() % is this how to do this? How does it know how many PMTs there are?
+%Record the state of all ScanImage settings we will change so we can change them back
+settings = mpsf.tools.recordScanImageSettings(API);
+
+API.turnOffPMTs % is this how to do this? How does it know how many PMTs there are?
 
 % Connect to Powermeter, set wavelength, zero
-    powermeter = mic.powermeter.PM100D;
-    powermeter.setWavelength(laser_wavelength) % sends new wavelength to powermeter
-    
-
-% control the laser power in percentage
-    API.controlLaserPower = 1; % set laser power to 1%
+powermeter = mpsf.interfaces.ThorPower; % IS THIS HOW TO CALL THE POWERMETER CLASS?
+powermeter.setWavelength(laser_wavelength) % sends new wavelength to powermeter
 
 % Tell SI to point
+hSI.scanPointBeam % CAN I DO THIS OR DOES IT NEED TO BE IN silinker?
+
+% control the laser power in percentage
+API.controlLaserPower = 1; % set laser power to 1%
 
 
-% measure power
-    Power = zeros(1,20);
-    Power(1) = powermeter.getPower;
-
-% save power, the percent power on SI, and what SI thinks it should be
+%% Measure power
+Power = zeros(10,20);
+for measurements = 1:size(Power,1) % measuring first data point
+    Power(measurements,1) = powermeter.getPower; % takes 10 measurements at each percentage, pausing for 0.25s between each
+    pause(0.25)
+end
 
 % then put it  in a loop!
-    for percent = 1:length(Power)-1 % should loop 19 times, first datapoint collected already 
-        API.controlLaserPower = percent*5; % increase laser power in 5% increments
-        pause(3); % pause for 3 seconds
-        Power(percent+1) =  Powermeter.getPower;
+for percent = 1:size(Power,2)-1 % should loop 19 times, first datapoint collected already
+    API.controlLaserPower = percent*5; % increase laser power in 5% increments
+    pause(3); % pause for 3 seconds
+    for measurements = 1:size(Power,1) % takes 10 measurements at each percentage, pausing for 0.25s between each
+        Power(measurements,percent+1) = powermeter.getPower;
+        pause(0.25)
     end
+    SIpower(percent+1) = % use BeamModulator.m - convertPowerFraction2PowerWatt  % the power scanimage thinks it is at each percentage laser power
+end
 
-  % Saving process
-  % Set file name and save dir
-    SETTINGS=mpsf.settings.readSettings;
-    fileStem = sprintf('%s_power_%dnm_%dmW_%s__%s', ...
-        SETTINGS.microscope.name, ...
-        laser_wavelength, ...
-        datestr(now,'yyyy-mm-dd_HH-MM-SS'));
+% Turn off point
 
-    API.hSI.hScan2D.logFileStem=fileStem;
-    API.hSI.hScan2D.logFilePath=saveDir;
-    API.hSI.hScan2D.logFileCounter=1;
 
-    API.acquireAndWait;
+%% Save measured power and what SI thinks it should be
 
-    mpsf.tools.reapplyScanImageSettings(API,settings);
+% Set file name and save dir
+SETTINGS=mpsf.settings.readSettings;
+fileStem = sprintf('%s_power_%dnm_%dmW_%s__%s', ...
+    SETTINGS.microscope.name, ...
+    laser_wavelength, ...
+    datestr(now,'yyyy-mm-dd_HH-MM-SS'));
 
-    % Report where the file was saved
-    mpsf.tools.reportFileSaveLocation(saveDir,fileStem)
 
-     % Save system settings to this location
-    settingsFilePath = mpsf.settings.findSettingsFile;
-    copyfile(settingsFilePath, saveDir)
+% API.hSI.hScan2D.logFileStem=fileStem;
+% API.hSI.hScan2D.logFilePath=saveDir;
+% API.hSI.hScan2D.logFileCounter=1;
+% 
+% API.acquireAndWait;
+
+
+% Report where the file was saved
+mpsf.tools.reportFileSaveLocation(saveDir,fileStem)
+
+% Save system settings to this location
+settingsFilePath = mpsf.settings.findSettingsFile;
+copyfile(settingsFilePath, saveDir)
+
+% Reapply original scanimage settings
+mpsf.tools.reapplyScanImageSettings(API,settings);
