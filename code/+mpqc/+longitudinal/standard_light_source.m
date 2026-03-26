e9910875a36d07262158763e259a2f5843dedfcf
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2026-03-26 17:20:56 +0000

    Added generic_generator_template function

--- code/+mpqc/+longitudinal/standard_light_source.m
+++ code/+mpqc/+longitudinal/standard_light_source.m
@@ -22,7 +22,7 @@ for ii=1:length(maintenanceFiles)
     if contains(tmp.name,'standard_light_source')
         plotting_template(n) = generic_generator_template(tmp);
         plotting_template(n).type = 'standard_light_source';
-        plotting_template(n).plotting_func = @mpqc.plot.electrical_noise;
+        plotting_template(n).plotting_func = @mpqc.plot.standard_light_source;
         plotting_template(n).date = string(datetime(regexp(tmp.name, '(\d{4}-\d{2}-\d{2})_(\d{2}-\d{2}-\d{2})','match'),'InputFormat','yyyy-MM-dd_HH-mm-ss'));
         [pathstr,plotting_template(n).name,ext] = fileparts(tmp.name);
         n=n+1;
@@ -51,6 +51,12 @@ if any(strcmp(varargin, 'startDate')) % Optional variable for selecting starting
     plotting_template = plotting_template(startIndex:end);
 end
 
+for ii = 1:length(plotting_template)
+    if contains(plotting_template(ii).full_path_to_data, '.tif')
+        stdLight(:,:,:,ii) = mpqc.tools.scanImage_stackLoad(plotting_template(ii).full_path_to_data); % (x,y,pmt,file)
+    end
+end
+
 % % TO DO
 % Read variables from title
 % Check standard light source serial number is same
@@ -60,6 +66,7 @@ end
 % 
 % Do we take the photon count analysis only?
 
+disp('done')
 
 
 
@@ -73,5 +80,12 @@ end
 
 
 
+end
 
+function out = generic_generator_template(t_dir)
+out.full_path_to_data = fullfile(t_dir.folder,t_dir.name);
+out.type = [];
+out.plotting_func = [];
+out.name = [];
+out.date = [];
 end
