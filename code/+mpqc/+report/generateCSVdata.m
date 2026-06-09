6bab265e654da0bb7b4ea1e7b0bfe2d62ed2b1d5
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2026-06-09 13:33:52 +0100

    First commit and framework for converting data to machine readable files for a html dashboard. Will be based on structure of mpqc.report.plot_functions_generator and generateMPQCreport

---
+++ code/+mpqc/+report/generateCSVdata.m
@@ -0,0 +1,41 @@
+function varargout=generateCSVdata(data_dir)
+% Creates machine readable data for html dashboard
+%
+%
+
+
+
+
+%% Longitudinal electrical noise
+
+
+
+
+
+
+
+%% Longitudinal power
+
+
+
+
+
+
+
+
+
+%% Longitudinal standard light source
+
+
+
+
+
+
+
+
+%% Longitudinal photons per pixel
+
+
+
+
+end
