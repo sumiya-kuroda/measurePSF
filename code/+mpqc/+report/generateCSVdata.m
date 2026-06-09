ff59bd62b682de8b937ef34381a58bef06e825b8
Isabell Whiteley <isabellwhiteley@gmail.com>
2026-06-09 19:10:47 +0100

    Should run all the longitudinal scripts and plot the data. The specific data outputs from the scripts and the dates should be loaded into a structure. This structure should then be converted to a table and then csv file. This still needs to be tested'

--- code/+mpqc/+report/generateCSVdata.m
+++ code/+mpqc/+report/generateCSVdata.m
@@ -4,15 +4,41 @@ function out=generateCSVdata(data_dir)
 %
 
 en = mpqc.longitudinal.electrical_noise(data_dir);
-out.electrical_noise = en.twoSD;
+if isempty(en)
+    return
+else
+    out.electrical_noise = en.twoSD;
+end
 
 pow = mpqc.longitudinal.power(data_dir);
-out.maxPower = pow.maxPower;
-out.percentAt100mW = pow.percentAt100mW;
+if isempty(pow)
+    return
+else
+    out.maxPower = pow.maxPower;
+    out.percentAt100mW = pow.percentAt100mW;
+    out.powerDate = pow.date;
+end
 % out.wavelength = {plotting_template(:).wavelength};
 
-% stdLight = mpqc.longitudinal.standard_light_source;
-% out.
+stdLight = mpqc.longitudinal.standard_light_source;
+if isempty(stdLight)
+    return
+else
+    out.meanValue = stdLight.meanValue;
+    out.stdLightDate = stdLight.date;
+end
+
+photons = mpqc.longitudinal.lens_paper;
+if isempty(photons)
+    return
+else
+    out.photonsPerPixel = photons.photonsPerPixel;
+end
+
+
+csvData = struct2table(out);
+outfile = fullfile(data_dir,'dashboardData.csv');
+writetable(csvData,outfile)
 
 
 function out = generic_generator_template(data_dir)
@@ -21,73 +47,3 @@ out.maxPower = [];
 out.percentAt100mW = [];
 
 
-% if nargin<1
-%     data_dir = pwd;
-% end
-% maintenanceFiles = [
-%     dir(fullfile(data_dir, '**', '*.tif'));
-%     dir(fullfile(data_dir, '**', '*.mat'))
-% ];
-% n=1;
-% standard_light_done = false;
-% 
-% % Searches for the files
-% for ii = 1:length(maintenanceFiles)
-%     tmp = maintenanceFiles(ii);
-% 
-%     if contains(tmp.name,'.tif') || contains(tmp.name,'.mat')
-%         if contains(tmp.name,'electrical_noise')
-%             out(n) = generic_generator_template(tmp);
-%             out(n).type = 'electrical_noise';
-%             out(n).plotting_func = @mpqc.longitudinal.electrical_noise;
-%             out(n).date = tmp.date;
-%             n=n+1;
-% 
-%         elseif contains(tmp.name,'lens_paper_')
-%             out(n) = generic_generator_template(tmp);
-%             out(n).type = 'lens_paper';
-%             out(n).plotting_func = @mpqc.longitudinal.lens_paper;
-%             out(n).date = tmp.date;
-%             n=n+1;
-% 
-%         elseif contains(tmp.name,'standard_light_source') && ~standard_light_done
-%             out(n) = generic_generator_template(tmp);
-%             out(n).type = 'standard_light_source';
-%             out(n).plotting_func = @mpqc.longitudinal.standard_light_source;
-%             out(n).data_dir = maintenanceFiles(ii).folder;
-%             out(n).date = tmp.date;
-%             n=n+1;
-% 
-%         elseif contains(tmp.name,'power')
-%             out(n) = generic_generator_template(tmp);
-%             out(n).type = 'power';
-%             out(n).plotting_func = @mpqc.longitudinal.power;
-%             out(n).date = tmp.date;
-%             n=n+1;
-%         end
-%     end
-% 
-%     if ~exist('out','var')
-%         fprintf('No data found in directory %s\n', data_dir)
-%         out = [];
-%     end
-% end
-% 
-% % Runs the functions
-% f=find(strcmp({out.type},'electrical_noise'));
-%     if ~isempty(f)
-%         for ii=1:length(f)
-%             data = out(f(ii)).plotting_func(out(f(ii)).full_path_to_data);
-%             electrical_noise = data.twoSD ;
-%         end  
-%     end
-% 
-% % Internal functions follow
-% function out = generic_generator_template(t_dir)
-% out.full_path_to_data = fullfile(t_dir.folder);
-% out.type = [];
-% out.plotting_func = [];
-% out.laser_wavelength = mpqc.report.laser_wavelength_from_fname(t_dir.name); %get laser wavelength
-% out.laser_power = mpqc.report.laser_power_from_fname(t_dir.name); %get laser power
-% out.data_dir = []; %for standard light source
-% out.date = [];
