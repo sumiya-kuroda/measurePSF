5e3e43b5c53b012a31651b62f4f08536b3638558
Isabell Whiteley <46966503+iwhiteley@users.noreply.github.com>
2026-06-10 17:34:24 +0100

    Function now outputs data as a csv file, but does in two different formats. This needs to be corrected after formatting is decided

--- code/+mpqc/+report/generateCSVdata.m
+++ code/+mpqc/+report/generateCSVdata.m
@@ -3,16 +3,20 @@ function out=generateCSVdata(data_dir)
 %
 %
 
+% debugging check
+maintenanceFiles = [dir(fullfile(data_dir,'\**\*.tif')) ; dir(fullfile(data_dir,'\**\*.mat'))];
+
 en = mpqc.longitudinal.electrical_noise(data_dir);
 if isempty(en)
-    return
+    disp('No electrical noise data')
 else
     out.electrical_noise = en.twoSD;
+    out.enDate = en.date;
 end
 
 pow = mpqc.longitudinal.power(data_dir);
 if isempty(pow)
-    return
+    disp('No power data')
 else
     out.maxPower = pow.maxPower;
     out.percentAt100mW = pow.percentAt100mW;
@@ -20,30 +24,31 @@ else
 end
 % out.wavelength = {plotting_template(:).wavelength};
 
-stdLight = mpqc.longitudinal.standard_light_source;
+stdLight = mpqc.longitudinal.standard_light_source(data_dir);
 if isempty(stdLight)
-    return
+    disp('No standard light source data')
 else
     out.meanValue = stdLight.meanValue;
     out.stdLightDate = stdLight.date;
 end
 
-photons = mpqc.longitudinal.lens_paper;
+photons = mpqc.longitudinal.lens_paper(data_dir);
 if isempty(photons)
-    return
+    disp('No photon counting data')
 else
     out.photonsPerPixel = photons.photonsPerPixel;
 end
 
 
-csvData = struct2table(out);
+csvData = struct2table(out,'AsArray',true);
+csvData = rows2vars(splitvars(csvData));
 outfile = fullfile(data_dir,'dashboardData.csv');
 writetable(csvData,outfile)
 
 
-function out = generic_generator_template(data_dir)
-out.electrical_noise = [];
-out.maxPower = [];
-out.percentAt100mW = [];
+% function out = generic_generator_template(data_dir)
+% out.electrical_noise = [];
+% out.maxPower = [];
+% out.percentAt100mW = [];
 
 
